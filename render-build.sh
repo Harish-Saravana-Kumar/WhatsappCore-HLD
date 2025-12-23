2ef355d1014fb0a5e0b906b36e20e3a7916b0dde
HarishS <harishs@gmail.com>
2025-12-23 11:40:59 +0530

    Use system Maven only in render-build.sh for Render compatibility

--- render-build.sh
+++ render-build.sh
@@ -3,16 +3,6 @@ set -e
 
 echo "Building WhatsApp Backend for Render..."
 
-# Install Maven
-MAVEN_VERSION=3.9.6
-wget https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
-tar -xzf apache-maven-${MAVEN_VERSION}-bin.tar.gz
-export PATH=$PWD/apache-maven-${MAVEN_VERSION}/bin:$PATH
-
-# Verify Maven installation
-mvn --version
-
-# Build the application
 cd backend
 mvn clean package -DskipTests
 
